a2fc34b299f07f8d53da5a92fa80c6f173525cca
Anastasia <1frolova.anastasia@gmail.com>
2017-09-25 00:09:32 +0300

    Initial commit

---
+++ src/main/java/WeatherForecast.java
@@ -0,0 +1,21 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by Anastasiaa on 9/24/2017.
+ */
+public class WeatherForecast {
+    public int getCurrentForecast()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void getThreeDaysForecast()
+    {
+        throw new NotImplementedException();
+    }
+
+    public String getCordinates()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/test/java/WeatherForecastTest.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Anastasiaa on 9/24/2017.
+ */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Before;
+
+public class WeatherForecastTest {
+
+    WeatherForecast weatherForecast;
+
+    @Before
+    public void setUp() throws Exception {
+        weatherForecast = new WeatherForecast();
+    }
+
+    @Test
+    public void testGetCurrentWeather() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testGetThreeDaysForecast() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testGeoCordinatesExists() {
+        assertTrue(false);
+    }
+}
